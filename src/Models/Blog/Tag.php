da0324830785442d1f89403bb0b2c1cccebf8703
Your Name <you@example.com>
2024-12-15 13:21:34 +0800

    Webkul\\GraphQLAPI\\Models\\Blog\\Blog

---
+++ src/Models/Blog/Tag.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Webkul\GraphQLAPI\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Webbycrown\BlogBagisto\Contracts\Tag as TagContract;
+
+class Tag extends Model implements TagContract
+{
+    use HasFactory;
+
+    protected $table = 'blog_tags';
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'status',
+        'locale',
+        'meta_title',
+        'meta_description',
+        'meta_keywords'
+    ];
+}
