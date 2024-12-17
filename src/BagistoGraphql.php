3429cb6ccafdfaacf1b7b9514bf30b621c14cde7
Your Name <you@example.com>
2024-12-17 15:25:23 +0800

    s

--- src/BagistoGraphql.php
+++ src/BagistoGraphql.php
@@ -98,19 +98,19 @@ class BagistoGraphql
 
         $user = auth()->guard($guard)->user();
 
-        // if (
-        //     isset($user->status)
-        //     && $user->status !== 1
-        // ) {
-        //     $message = trans('bagisto_graphql::app.shop.customers.login.not-activated');
-        // }
+        if (
+            isset($user->status)
+            && $user->status !== 1
+        ) {
+            $message = trans('bagisto_graphql::app.shop.customers.login.not-activated');
+        }
 
-        // if (
-        //     isset($user->is_verified)
-        //     && $user->is_verified !== 1
-        // ) {
-        //     $message = trans('bagisto_graphql::app.shop.customers.login.verify-first');
-        // }
+        if (
+            isset($user->is_verified)
+            && $user->is_verified !== 1
+        ) {
+            $message = trans('bagisto_graphql::app.shop.customers.login.verify-first');
+        }
 
         // if (
         //     isset($user->is_suspended)
@@ -119,15 +119,15 @@ class BagistoGraphql
         //     $message = trans('bagisto_graphql::app.shop.customers.login.suspended');
         // }
 
-        // if (isset($message)) {
-        //     if ($token) {
-        //         request()->merge(['token' => $token]);
-        //     }
+        if (isset($message)) {
+            if ($token) {
+                request()->merge(['token' => $token]);
+            }
 
-        //     auth()->guard($guard)->logout();
+            auth()->guard($guard)->logout();
 
-        //     throw new CustomException($message);
-        // }
+            throw new CustomException($message);
+        }
 
         return $user;
     }
